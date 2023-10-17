8f48b3b9abb93192e5de71094b18b18945898814
Dalia <93775255+DaliaAlshahat@users.noreply.github.com>
2023-10-17 20:50:35 +0300

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,14 @@
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)  # double slashes to get an integer output
+print(10 % 3)  # the remainder of a division
+print(10 ** 3)
+
+x = 10
+print(x)
+x = x + 3  # augmented assignment operator
+print(x)
+x += 3
+print(x)
